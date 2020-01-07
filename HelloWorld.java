aa4a862a8f99a3c16bca929616aed1c0dcb4bbcc
=blue-coast <1746007300@qq.com>
2020-01-07 17:27:34 +0800

    other helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String[] args) {
        System.out.println("1");
        System.out.println("Hello World");
-       System.out.println("2");
+       System.out.println("2")£»
        System.out.println("Hello World");
     }
 }
